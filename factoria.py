b0b290f7c92870d4ff9b22724e7dea6c9c3a0e25
BlIsteinar27 <sulimarvelasquez@gmail.com>
2024-07-09 11:45:34 -0400

    version 2

--- factoria.py
+++ factoria.py
@@ -1,7 +1,7 @@
 #programa que determina secuencias por repeticion
 #elaborado por:Diego Yepez                                                                       fecha02/07/20224
 #var. de entrada
-num_n = int
+numero = int
 #var. de salida
 factoria_numero_n = int
 #var. de proceso
@@ -9,8 +9,8 @@ factoria = 1
 #lectura de datos
 #inicializacion de variablees
 #procesamiento de datos
-num_n = int(input("ingrese numero entero positivo: "))
-for k in range(1,num_n + 1):
+numero = int(input("ingrese numero entero positivo: "))
+for k in range(1,numero + 1):
     factoria = factoria * k
     
 #escritura de resultados
